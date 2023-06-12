991934574bbd915a2e21c1898c3910344f761212
aleizn <119465229+aleizn@users.noreply.github.com>
2023-06-11 22:40:23 -0500

    Aleizn

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -4,12 +4,11 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `《 𝐌𝐞𝐧𝐬𝐚𝐣𝐞 》: ${pesan}`
-let teks = `Bot creado por Wa.me/51992621601 prohibo la venta del bot
-《😼𝐈𝐍𝐕𝐎𝐂𝐀𝐂𝐈𝐎𝐍 𝐆𝐄𝐍𝐄𝐑𝐀𝐋 》\n\n❏ ${oi}\n\n❏ 《 𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚𝐬 》:\n`
+let oi = `《《 𝐌𝐞𝐧𝐬𝐚𝐣𝐞 》:  Buenos Días / Tardes / Noches... 🤗🤗 : ${pesan}`
+let teks = `《🥰 𝐕𝐁 𝐍𝐈𝐊𝐊𝐈 𝐋𝐎𝐒 𝐈𝐍𝐕𝐎𝐂𝐀 𝐏𝐑𝐄𝐂𝐈𝐎𝐒𝐎𝐒 🫶🥰, 𝐍𝐎 𝐒𝐄𝐀𝐍 𝐏𝐋𝐀𝐍𝐓𝐈𝐓𝐀𝐒》\n\n❏ ${oi}\n\n❏ 《 𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚𝐬 》:\n`
 for (let mem of participants) {
-teks += `✼❤️‍🔥 @${mem.id.split('@')[0]}\n`}
-teks += `➥𝐀𝐥𝐞𝐢𝐳𝐧-𝐌𝐃`
+teks += `✼🌟 @${mem.id.split('@')[0]}\n`}
+teks += `➥𝐏𝐫𝐨𝐩𝐢𝐞𝐝𝐚𝐝 𝐝𝐞 𝐍𝐢𝐤𝐤𝐢 💞`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
